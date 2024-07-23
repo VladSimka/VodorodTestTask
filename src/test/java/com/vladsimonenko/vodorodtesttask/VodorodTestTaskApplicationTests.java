d3dfb8bb71861647a9539882f3bf2617dc6aaba7
Vladislav Simonenko <vlad.simonenko.83@mail.ru>
2024-07-23 13:15:54 +0300

    init commit

---
+++ src/test/java/com/vladsimonenko/vodorodtesttask/VodorodTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladsimonenko.vodorodtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodorodTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
